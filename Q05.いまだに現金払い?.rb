5a512293f4d3530b56aa55dafafb404e5b4f068d
y.yoshiki <labiatejoker@gmail.com>
2017-08-11 14:57:27 +0900

    Create answer

--- Q05.いまだに現金払い?.rb
+++ Q05.いまだに現金払い?.rb
@@ -13,9 +13,9 @@ count = 0
   (0..10).each do |coin100|
     (0..15).each do |coin50|
       (0..15).each do |coin10|
-        if coin500 + coin100 + coin50 + coin10 <= 15
-          if coin500*500 + coin100*100 + coin50*50 + coin10*10 == 1000
-            count += 1
+        if coin500 + coin100 + coin50 + coin10 <= 15 #15枚以内について判定
+          if coin500*500 + coin100*100 + coin50*50 + coin10*10 == 1000 #各枚数に値を掛ける
+            count += 1 #TRUEの値についてはcountに＋1を追加
           end
         end
       end
@@ -24,3 +24,20 @@ count = 0
 end
 
 puts count
+
+# ANSWER2
+
+coins = [10, 50, 100, 500]
+count = 0
+
+(2..15).each do |i|
+  coins.repeated_combination(i).each{|coin_set|
+    count += 1 if coin_set.inject(:+) == 1000
+  }
+end
+
+puts count
+
+# ANSWER3
+
+
