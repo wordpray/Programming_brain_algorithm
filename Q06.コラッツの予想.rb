df1e9d44b48ad5086a8cb547fe4d46cbe2714827
y.yoshiki <labiatejoker@gmail.com>
2017-08-12 16:21:42 +0900

    Create answer

--- Q06.コラッツの予想.rb
+++ Q06.コラッツの予想.rb
@@ -7,14 +7,12 @@ def is_loop(n)
   #1になるまで数字を変化させることを繰り返す
   while check != 1 do
     check = check.even? ? check/2 : check *3 + 1
-    return true if check ==n
+    return true if check == n
   end
   return false
 end
 
-
 #2−10000まで、偶数についてチェックする
-
 puts 2.step(10000, 2).count{|i|
   is_loop(i)
 }
